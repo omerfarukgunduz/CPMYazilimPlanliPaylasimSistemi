f733a9b109733573e24ef115d29c7760cdc9f6ca
Berke Yıldırım <berke-yildirim-2580@hotmail.com>
2023-08-08 17:00:36 +0300

    login alert

---
+++ Portal.Web/ViewModel/HomeIndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Portal.Web.ViewModel
+{
+    public class HomeIndexViewModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public bool HasError { get; set; }
+        public string Error { get; set; }
+        public Guid Id { get; set; }
+    }
+}
