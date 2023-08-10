f5dbed8a62415992e0e068fa252a9457c319f9df
Berke Yıldırım <berke-yildirim-2580@hotmail.com>
2023-08-10 11:05:40 +0300

    mig_5

--- Portal.Web/ViewModel/EtkinlikEkleViewModel.cs
+++ Portal.Web/ViewModel/EtkinlikEkleViewModel.cs
@@ -2,5 +2,12 @@
 {
 	public class EtkinlikEkleViewModel
 	{
+		public string? title { get; set; }
+		public string? description { get; set; }
+		public DateTime? start { get; set; }
+		public DateTime? end { get; set; }
+		public IFormFile image { get; set; }
+		public bool whatsapp { get; set; }
+		public bool linkedin { get; set; }
 	}
 }
