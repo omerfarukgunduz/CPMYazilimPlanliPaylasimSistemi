ce968aef0a17e5384e0f674a9efc1f7d146c9f04
Berke Yıldırım <berke-yildirim-2580@hotmail.com>
2023-08-14 13:38:50 +0300

    tıklanan etkinlik

--- Portal.Domain/Entities/Etkinlik.cs
+++ Portal.Domain/Entities/Etkinlik.cs
@@ -6,7 +6,7 @@ namespace Portal.Domain.Entities
     {
         public string? title { get; set; }
         public string? description { get; set; }
-        public DateTime? start { get; set; }
+        public DateTime start { get; set; }
         public DateTime? end { get; set; }
         public string? image { get; set; }
         public bool? whatsapp { get; set; }
--- Portal.Web/ViewModel/EtkinlikEkleViewModel.cs
+++ Portal.Web/ViewModel/EtkinlikEkleViewModel.cs
@@ -4,7 +4,7 @@
 	{
 		public string? title { get; set; }
 		public string? description { get; set; }
-		public DateTime? start { get; set; }
+		public DateTime start { get; set; }
 		public DateTime? end { get; set; }
 		public IFormFile image { get; set; }
 		public bool whatsapp { get; set; }
